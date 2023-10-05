be8808a7bedfa36ffb74ecc73f923c4376a7a3c3
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-10-05 16:11:58 +0530

    Fixed some minor bugs related to proxy and used baseURL there

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import express from "express";
 import "dotenv/config";
 import "./config/dbConfig.js";
 import usersRoute from "./routes/usersRoute.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
